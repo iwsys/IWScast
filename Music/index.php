da610858e09a328e9b72ae0db0c2f34e8b790e53
Igor <iwsys@yandex.ru>
2019-10-09 12:43:39 +0300

    Create index.php
    Shuffle files

---
+++ Music/index.php
@@ -0,0 +1,40 @@
+<?php
+set_time_limit(0);
+header('Content-type: audio/mpeg');
+header ("Content-Transfer-Encoding: binary");
+header ("Pragma: no-cache");
+header ("icy-br: 128 ");
+header ("icy-name: your name");
+header ("icy-description: your description"); 
+
+$dirPath = "";
+
+$files = glob("*.mp3");
+shuffle($files); //Random on
+
+for ($x=0; $x < count($files);) {
+  $filePath =  $files[$x++];
+  $bitrate = 128;
+  $strContext=stream_context_create(
+   array(
+     'http'=>array(
+       'method' =>'GET',
+       'header' => 'Icy-MetaData: 1',
+       'header' =>"Accept-language: en\r\n"
+       )
+     )
+   );
+
+//Save to log 
+  $fl = $filePath; 
+  $log = date('Y-m-d H:i:s') . ' Song - ' . $fl;
+  file_put_contents('log.txt', $log . PHP_EOL, FILE_APPEND);
+  $fpOrigin=fopen($filePath, 'rb', false, $strContext);
+  while(!feof($fpOrigin)){
+   $buffer=fread($fpOrigin, 4096);
+   echo $buffer;
+   flush();
+ }
+ fclose($fpOrigin);
+}
+?>
